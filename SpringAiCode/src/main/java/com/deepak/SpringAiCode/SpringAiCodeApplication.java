4ec7d3adbed471a38f7c53a812adb689cfb3edcc
Deepak Kumar <deepak.naman2002@gmail.com>
2025-09-30 00:49:56 +0530

    30/09/2025
    9

--- SpringAiCode/src/main/java/com/deepak/SpringAiCode/SpringAiCodeApplication.java
+++ SpringAiCode/src/main/java/com/deepak/SpringAiCode/SpringAiCodeApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAiCodeApplication {
-
+public class SpringAiCodeApplication
+{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAiCodeApplication.class, args);
 	}
